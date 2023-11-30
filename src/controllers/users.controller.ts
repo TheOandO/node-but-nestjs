fa2d0911a32789293fa3cbf08a51ffe3e34f56c1
Trịnh Khánh <realkripper@gmail.com>
2023-11-30 16:06:42 +0700

    update mailer

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,17 +5,15 @@ import { UpdateUserDto } from 'src/dto/update-user.dto';
 import { JoiValidationPipe } from '../pipes/joi-validation.pipe';
 import { createUserSchema } from '../schemas/joi-schema';
 import { JwtAuthGuard } from 'src/guards/auth.guard';
-import { MailService } from 'src/services/mail.service';
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService, private readonly mailService:MailService) {}
+    constructor(private readonly userService: UserService) {}
 
     @Post()
     @UseGuards(JwtAuthGuard)
     @UsePipes(new JoiValidationPipe(createUserSchema))
     create(@Body() createUserDto: CreateUserDto) {
-        this.mailService.sendMailWelcomeUser(createUserDto.email, { name: createUserDto.username })
         return this.userService.create(createUserDto);
     };
 
@@ -34,7 +32,6 @@ export class UserController {
     @Put(':id')
     @UseGuards(JwtAuthGuard)
     update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        this.mailService.sendMailUpdate(updateUserDto.email, { name: updateUserDto.username })
         return this.userService.update(id, updateUserDto);
     };
 
